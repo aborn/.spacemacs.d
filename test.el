ea4b9ecc68f2d6848ea1b05602813ac03903957e
蒋国宝 <loveaborn@foxmail.com>
2016-05-16 22:51:22 +0800

    update

--- test.el
+++ test.el
@@ -1,5 +1,14 @@
-(defun test-butle (arg)  
+(defun test-butle (arg)
   "only used for ws-butler-global-mode test"
   (interactive "p")
   (message "turn off ws-butler-global-mode")
   (ws-butler-global-mode -1))
+
+
+(zzz-to-char .
+             [(20160122 440)
+              ((emacs (24 4)) (cl-lib (0 5)) (avy (0 3 0)))
+              "Fancy version of `zap-to-char' command"
+              single
+              ((:url . "https://github.com/mrkkrp/zzz-to-char") (:keywords "convenience"))
+              ])
