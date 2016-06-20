08d1f40c8f572cd42df647e539db67111c59ad88
Aborn Jiang <loveaborn@foxmail.com>
2016-06-20 23:35:58 +0800

    json-mode

--- init.el
+++ init.el
@@ -55,6 +55,7 @@ values."
    dotspacemacs-additional-packages '(
                                       tree-mode
                                       wgrep
+                                      json-mode
                                       )
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
