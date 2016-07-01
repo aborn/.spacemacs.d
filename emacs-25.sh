143f403adcf2689e025d3df4ff5b654336ccb72e
蒋国宝 <loveaborn@foxmail.com>
2016-07-01 20:21:26 +0800

    add emacs 25

---
+++ emacs-25.sh
@@ -0,0 +1,17 @@
+wget ftp://alpha.gnu.org/gnu/emacs/pretest/emacs-25.0.95.tar.xz
+# unzip
+xz -d emacs-25.0.95.tar.xz
+tar -xvf emacs-25.0.95.tar
+cd emacs-25.0.95/
+# install depends
+sudo apt-get install libgtk-3-dev #first
+# configure: error: xwidgets requested but WebKitGTK+ not found
+sudo apt-get install libwebkitgtk-dev
+sudo apt-get install libwebkitgtk-3.0-dev
+
+./configure --with-xwidgets  --with-x-toolkit=gtk3
+make
+sudo make install
+
+# open emacs
+# M-x xwidget-webkit-browse-url
