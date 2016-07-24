c4f7f7f62515bb83354688aa33417b8f2bdb3d0b
Aborn Jiang <loveaborn@foxmail.com>
2016-07-24 19:01:22 +0800

    git commit push

--- parts/aborn-swift.el
+++ parts/aborn-swift.el
@@ -1,12 +1,14 @@
 ;; some swift action
 
-(defun aborn/swift-git-commit-push (&optional commit)
-  "commit and push"
-  (interactive)
-  (when (null commit)
-    (setq commit (read-string "your commit: " nil nil "update")))
-  (message "your commit is %s" commit)
+(defun aborn/swift-git-commit-push (message)
+  "message and push"
+  (interactive "sCommit Message: ")
+  (when (null message)
+    (setq message (read-string "your message: " nil nil "update")))
+  (message "your message is %s" message)
   (save-buffer)
-  )
+  (magit-stage-modified)
+  (magit-commit (list "-m" message))
+  ) 
 
 (provide 'aborn-swift)
