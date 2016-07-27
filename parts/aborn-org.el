263117df22cd8fbb7aaf0687a26b89c0149fefd6
guobao.jiang <loveaborn@foxmail.com>
2016-07-27 16:48:22 +0800

    org-agenda-timeline-all

--- parts/aborn-org.el
+++ parts/aborn-org.el
@@ -15,4 +15,15 @@
   (setq org-mobile-directory "/Users/aborn/github/iGTD/mobile")
   (setq org-mobile-inbox-for-pull "/Users/aborn/github/iGTD/from-mobile.org"))
 
+(defun org-agenda-timeline-all (&optional arg)
+  (interactive "P")
+  (with-temp-buffer
+    (dolist (org-agenda-file org-agenda-files)
+      (insert-file-contents org-agenda-file nil)
+      (end-of-buffer)
+      (newline))
+    (write-file "/tmp/timeline.org")
+    (org-agenda arg "L")))
+(define-key org-mode-map (kbd "C-c t") 'org-agenda-timeline-all)
+
 (provide 'aborn-org)
