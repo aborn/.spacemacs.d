5acb3de4c8d99c3b73b2af0810607d8af889e4d0
Aborn Jiang <loveaborn@foxmail.com>
2016-07-29 00:41:38 +0800

    hyperbole added

--- init.el
+++ init.el
@@ -69,6 +69,7 @@ values."
                                       highlight-symbol
                                       beacon
                                       buttercup
+                                      hyperbole
                                       v2ex-mode
                                       )
    ;; A list of packages and/or extensions that will not be install and loaded.
