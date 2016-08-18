1b9d6df4178fdbdbbb7e68857b8db22a4b4de790
guobao.jiang <loveaborn@foxmail.com>
2016-08-18 18:39:20 +0800

    delete markdown-file use spacemeacs default

--- init-aborn.el
+++ init-aborn.el
@@ -133,9 +133,6 @@
 ;; set markdown-mode download from
 ;;                    http://jblevins.org/projects/markdown-mode/
 ;; -----------------------------------------------------------------------------
-(load-file "~/.spacemacs.d/modules/markdown-mode.el")
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
