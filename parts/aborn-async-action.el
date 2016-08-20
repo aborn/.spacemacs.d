779226358246f077f8ed2449ae47ab4ec999745c
Aborn Jiang <loveaborn@foxmail.com>
2016-08-20 20:34:35 +0800

    aborn/git-code-update modify

--- parts/aborn-async-action.el
+++ parts/aborn-async-action.el
@@ -1,28 +1,29 @@
-;; some async actions, which will cost some time.
+;; some async actions, which will cost some time running in background.
+(defvar aborn-git-project-list
+  '("~/.emacs.d/" "popkit" "~/.spacemacs.d/" "piece-meal" "pelpa"
+    "eden" "leanote-mode" "v2ex-mode" "learn-elisp-by-examples"
+    "aborn-multi-term"))
+
 (defun aborn/git-code-update ()
   "update code async."
   (interactive)
   (async-start
    ;; 异步执行更新code操作
-   (lambda ()
-     (add-to-list 'load-path "~/.spacemacs.d/parts")
-     (require 'aborn-log)
-     (require 'subr-x)
-     (aborn/log "exec-when-emacs-boot....")
-     (let ((ab--git-project-list
-            '("~/.emacs.d/" "popkit" "~/.spacemacs.d/" "piece-meal" "pelpa"
-              "eden" "leanote-mode" "v2ex-mode" "learn-elisp-by-examples"
-              "aborn-multi-term")))
-       (dolist (elt ab--git-project-list)
-         (let* ((working-directory
-                 (if (or (string-prefix-p "/" elt) (string-prefix-p "~" elt))
-                     elt
-                   (concat "~/github/" elt "/")))
-                (default-directory working-directory))
-           (aborn/log (shell-command-to-string "echo $PWD"))
-           ;; 执行操作是异步的!
-           (aborn/log (shell-command-to-string "git pull")))))
-     (aborn/log "finished aborn/git-code-update."))
+   `(lambda ()
+      ,(async-inject-variables "\\`load-path\\'")
+      (require 'aborn-log)
+      (require 'subr-x)
+      (aborn/log "exec-when-emacs-boot....")
+      (dolist (elt aborn-git-project-list)
+        (let* ((working-directory
+                (if (or (string-prefix-p "/" elt) (string-prefix-p "~" elt))
+                    elt
+                  (concat "~/github/" elt "/")))
+               (default-directory working-directory))
+          (aborn/log (shell-command-to-string "echo $PWD"))
+          ;; 执行操作是异步的!
+          (aborn/log (shell-command-to-string "git pull"))))
+      (aborn/log "finished aborn/git-code-update."))
    (lambda (result)
      (message "finished aborn/git-code-update. %s" result))))
 
