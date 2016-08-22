124457f24ae7a94f6d2790a24475ee57537fc8f9
Aborn Jiang <loveaborn@foxmail.com>
2016-08-22 15:38:38 +0800

    update@2016-08-22 15:38:37

--- parts/aborn-swift.el
+++ parts/aborn-swift.el
@@ -8,11 +8,21 @@
   (save-buffer)
   (magit-stage-modified)
   (magit-commit (list "-m" msg))
-  (magit-push-current-to-upstream)
-  ;; (aborn/timer-task-delay-excute-once
-  ;;  1       ;; 延时1s执行 git push操作
-  ;;  (lambda ()
-  ;;    (call-interactively #'magit-push-current-to-upstream)))
-  )
+  (let (exec-path-current)
+    (setq exec-path-current default-directory)
+    (async-start
+     `(lambda ()
+        (setq default-directory ,exec-path-current)
+        ,(async-inject-variables "\\`load-path\\'") ;; add main process load-path
+        (require 'aborn-timer-task)
+        (require 'aborn-log)
+        (aborn/log ("exec push in %s" default-directory))
+        (aborn/timer-task-delay-excute-once
+         1       ;; 延时1s执行 git push操作
+         (lambda ()
+           (call-interactively #'magit-push-current-to-upstream))))
+     (lambda (result)
+       (message "push to upstream success. %s:%s" exec-path-current result))
+     )))
 
 (provide 'aborn-swift)
