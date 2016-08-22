3ca6db853a257d953e085f6f3d2973121915dc21
Aborn Jiang <loveaborn@foxmail.com>
2016-08-22 15:44:10 +0800

    update@2016-08-22 15:44:09

--- parts/aborn-swift.el
+++ parts/aborn-swift.el
@@ -8,21 +8,21 @@
   (save-buffer)
   (magit-stage-modified)
   (magit-commit (list "-m" msg))
-  (let (exec-path-current)
-    (setq exec-path-current default-directory)
-    (async-start
-     `(lambda ()
-        ,(async-inject-variables "\\`default-directory\\'")
-        ,(async-inject-variables "\\`load-path\\'") ;; add main process load-path
-        (require 'aborn-timer-task)
-        (require 'aborn-log)
-        (aborn/log (format "exec push in %s" default-directory))
-        (aborn/timer-task-delay-excute-once
-         1       ;; 延时1s执行 git push操作
-         (lambda ()
-           (call-interactively #'magit-push-current-to-upstream))))
-     (lambda (result)
-       (message "push to upstream success. %s:%s" exec-path-current result))
-     )))
+  (setq exec-path-current default-directory)
+  (async-start
+   `(lambda ()
+      ,(async-inject-variables "\\`default-directory\\'")
+      ,(async-inject-variables "\\`load-path\\'") ;; add main process load-path
+      (require 'aborn-timer-task)
+      (require 'aborn-log)
+      (aborn/log (format "exec push in %s" default-directory))
+      (aborn/timer-task-delay-excute-once
+       1       ;; 延时1s执行 git push操作
+       (lambda ()
+         (call-interactively #'magit-push-current-to-upstream)))
+      default-directory)
+   (lambda (result)
+     (message "push to upstream success. %s" result))
+   ))
 
 (provide 'aborn-swift)
