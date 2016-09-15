4ace5883f105f54a1f314aee32e84125a367768d
Aborn Jiang <loveaborn@foxmail.com>
2016-09-15 22:58:22 +0800

    finished current elisp func

--- parts/aborn-utils.el
+++ parts/aborn-utils.el
@@ -85,16 +85,19 @@
 (defun aborn/current-elisp-functions ()
   "Get current elisp function defs"
   (interactive)
-  (save-excursion
-    (beginning-of-buffer)
-    (while (not (eobp))
-      (let ((ft (re-search-forward "(defun\s+"))
-            (fe))
-        (when ft
-          (setq fe (re-search-forward "\s"))
-          (when fe
-            (message "^%s %s" (line-number-at-pos) (buffer-substring ft fe))))
-        ))))
+  (let (result)
+    (save-excursion
+      (beginning-of-buffer)
+      (while (and (not (eobp))
+                  (string-match "(defun\s+" (buffer-substring (point) (point-max))))
+        (let ((ft (re-search-forward "(defun\s+"))
+              (fe (re-search-forward "\s")))
+          (when (and ft fe)
+            (message "%s  %s" (line-number-at-pos) (buffer-substring ft fe))
+            (add-to-list 'result `(,(line-number-at-pos) . ,(s-trim (buffer-substring-no-properties ft fe))))
+            )
+          )))
+    result))
 
 (provide 'aborn-utils)
 ;;; aborn-utils.el ends here
