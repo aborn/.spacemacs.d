9662d7d80ac51632263f38b083c0f2c84ceaf2c9
Aborn Jiang <loveaborn@foxmail.com>
2016-09-25 10:46:05 +0800

    custom-file to ~/.spacemacs.d/custom.el

--- init.el
+++ init.el
@@ -302,6 +302,7 @@ user code here.  The exception is org related code, which should be placed in
   "Configuration function for user code.
 This function is called at the very end of Spacemacs initialization after
 layers configuration. You are free to put any user code."
+  (setq custom-file "~/.spacemacs.d/custom.el")
   (add-to-list 'load-path "~/.spacemacs.d/parts")
   (add-to-list 'load-path "~/.spacemacs.d/modules")
   (add-to-list 'load-path "~/.spacemacs.d/hotkey")
