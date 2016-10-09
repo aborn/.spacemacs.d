5e69a762e56af34b973dec623fe1a1915cea8fd3
guobao.jiang <loveaborn@foxmail.com>
2016-10-09 13:24:00 +0800

    add custom.el

--- init.el
+++ init.el
@@ -313,30 +313,3 @@ layers configuration. You are free to put any user code."
   (load-file "~/.spacemacs.d/init-aborn.el")
   (load-file "~/.spacemacs.d/hotkey/aborn-prefix.el")
   )
-
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/github/eden/dianping/SharkPush.org" "~/github/eden/career/appkit-buglist.org" "~/github/leanote-mode/docs/leanote-dev-plan.org" "~/github/eden/dianping/agenda.org" "~/.spacemacs.d/docs/aborn-emacs-todo.org" "~/github/eden/plan/reading2016/reading-plan.org")))
- '(package-selected-packages
-   (quote
-    (mustache evil-visual-mark-mode yapfify xterm-color uuidgen undo-tree fifo-class powerline smartparens rake faceup py-isort pcre2el org-projectile alert gntp log4e org git ht org-download mwim markdown-mode livid-mode skewer-mode simple-httpd live-py-mode link-hint pcache json-reformat json-snatcher js2-mode htmlize parent-mode smex projectile request haml-mode gitignore-mode git-link fringe-helper git-gutter+ git-gutter flyspell-correct-helm flyspell-correct pos-tip flycheck-mix flycheck flx ivy eyebrowse evil-unimpaired magit git-commit magit-popup with-editor iedit anzu evil eshell-z dumb-jump diminish web-completion-data tern dash-functional company-shell go-mode company-emacs-eclim column-enforce-mode color-identifiers-mode clojure-snippets hydra paredit multiple-cursors edn inflections peg eval-sexp-fu highlight cider clojure-mode queue spinner inf-ruby bind-map bind-key seq yasnippet packed anaconda-mode pythonic f s elixir-mode dash pkg-info epl pinyinlib ace-jump-mode helm avy helm-core async auto-complete popup package-build company wttrin ws-butler window-numbering which-key wgrep web-mode web-beautify wakatime-mode volatile-highlights vi-tilde-fringe v2ex-mode use-package tree-mode toc-org timp tagedit swiper stickyfunc-enhance srefactor sql-indent spacemacs-theme spaceline smooth-scrolling smeargle slim-mode shell-pop scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-end rubocop rspec-mode robe restart-emacs rbenv rainbow-mode rainbow-identifiers rainbow-delimiters racket-mode quelpa pyvenv pytest pyenv-mode py-yapf popwin pip-requirements persp-mode paradox page-break-lines orgit org-repo-todo org-present org-pomodoro org-plus-contrib org-page org-bullets open-junk-file neotree multi-term move-text mmm-mode markdown-toc magit-gitflow macrostep lorem-ipsum linum-relative leuven-theme less-css-mode leanote kanban json-mode js2-refactor js-doc jade-mode ivy-smex info+ indent-guide ido-vertical-mode hyperbole hy-mode hungry-delete hl-todo highlight-symbol highlight-parentheses highlight-numbers highlight-indentation highlight-defined help-fns+ helm-themes helm-swoop helm-smex helm-pydoc helm-projectile helm-org-rifle helm-mode-manager helm-make helm-gtags helm-gitignore helm-github-stars helm-flyspell helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag hackernews google-translate golden-ratio go-eldoc gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-gutter-fringe git-gutter-fringe+ gh-md ggtags format-sql flycheck-pos-tip flx-ido fish-mode find-file-in-project fill-column-indicator fancy-battery f3 expand-region exec-path-from-shell evil-visualstar evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-args evil-anzu eshell-prompt-extras esh-help erlang emmet-mode elisp-slime-nav eclim easy-kill disable-mouse diff-hl deft define-word cython-mode company-web company-tern company-statistics company-quickhelp company-go company-anaconda column-marker coffee-mode clj-refactor clean-aindent-mode cider-eval-sexp-fu chruby buttercup bundler buffer-move bracketed-paste beacon auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile all-the-icons alchemist aggressive-indent adaptive-wrap ace-window ace-pinyin ace-link ace-jump-helm-line ac-ispell)))
- '(wakatime-api-key "e191899f-9e2a-4d00-8100-4e9f9523decb"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "nil" :slant normal :weight normal :height 150 :width normal))))
- '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
- '(highlight-defined-builtin-function-name-face ((((class color) (min-colors 88) (background dark)) :foreground "#7b875b")))
- '(highlight-defined-function-name-face ((((class color) (min-colors 88)) :foreground "#997599")))
- '(highlight-defined-special-form-name-face ((((class color) (min-colors 88)) :foreground "#626262")))
- '(highlight-defined-variable-name-face ((((class color) (min-colors 88)) :foreground "#8d5351"))))
