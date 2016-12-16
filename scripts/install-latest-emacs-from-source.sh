641784583e37d23f81ec783afa4f847c08fc8578
Aborn Jiang <loveaborn@foxmail.com>
2016-12-17 00:51:29 +0800

    dep gnutls

--- scripts/install-latest-emacs-from-source.sh
+++ scripts/install-latest-emacs-from-source.sh
@@ -2,10 +2,14 @@
 ##########################################################################
 # Install latest emacs from emacs source code
 # by Aborn Jiang (aborn.jiang@gmail.com)
+# for mac builds: https://emacsformacosx.com/builds
+# I did this in mac.
+#   1. brew install gnutls
 ##########################################################################
 
 ROOTPATH="${HOME}/github/emacs"
 
+# clone source code first
 if [ ! -d "$ROOTPATH" ]; then
     # git clone git://git.savannah.gnu.org/emacs.git  # All history
     git clone --depth 1 git://git.sv.gnu.org/emacs.git ${ROOTPATH}
@@ -13,3 +17,10 @@ else
     cd ${ROOTPATH}
     git pull
 fi
+
+# configure & compile & install
+cd ${ROOTPATH}
+./autogen.sh
+./configure
+make
+make install
