89884e4d374f0807dc5676b9d527510836d4e238
Aborn Jiang <loveaborn@foxmail.com>
2016-12-17 00:06:51 +0800

    add $HOME

--- scripts/install-latest-emacs-from-source.sh
+++ scripts/install-latest-emacs-from-source.sh
@@ -4,5 +4,12 @@
 # by Aborn Jiang (aborn.jiang@gmail.com)
 ##########################################################################
 
-ROOTPATH="~/github/emacs"
-git clone --depth 1 git://git.sv.gnu.org/emacs.git ${ROOTPATH}
+ROOTPATH="${HOME}/github/emacs"
+
+if [ ! -d "$ROOTPATH" ]; then
+    # git clone git://git.savannah.gnu.org/emacs.git  # All history
+    git clone --depth 1 git://git.sv.gnu.org/emacs.git ${ROOTPATH}
+else
+    cd ${ROOTPATH}
+    git pull
+fi
