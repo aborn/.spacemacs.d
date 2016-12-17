98e1f8f5575fdace6f7da9107126fbb379332347
Aborn Jiang <loveaborn@foxmail.com>
2016-12-17 23:56:41 +0800

    emacs 26 for thread

--- demo/exp.el
+++ demo/exp.el
@@ -79,3 +79,34 @@
     (message "Input value is 「%s」." input-value)))
 
 (setq neo-hidden-regexp-list '("^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$" ".el$"))
+
+;; following only can apply in emacs 26
+(defmacro define-background-function-wrapper (bg-function fn)
+  (let ((is-loading-sym (intern (concat "*" (symbol-name bg-function) "-is-loading*"))))
+    `(progn
+       (defvar ,is-loading-sym nil)
+       (defun ,bg-function ()
+         (interactive)
+         (when ,is-loading-sym
+           (message ,(concat (symbol-name fn) " is already loading")))
+         (setq ,is-loading-sym t)
+         (make-thread (lambda ()
+                        (unwind-protect
+                            (,fn)
+                          (setq ,is-loading-sym nil))))))))
+
+(defun threadaction ()
+  ""
+  (insert "I am running in bg")
+  (insert "\n"))
+
+(define-background-function-wrapper bg-threadaction threadaction)
+
+
+(make-thread (lambda ()
+               (message "running in bg.")))
+
+(make-thread (lambda ()
+               (sit-for 5)
+               (with-current-buffer "z"
+                 (insert "foo"))))
