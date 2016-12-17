b75bac2355f296f2bc3d1e81df986f33d230eb37
Aborn Jiang <loveaborn@foxmail.com>
2016-12-18 00:03:36 +0800

    commit by magit in emacs@2016-12-18 00:03:36

--- scripts/install-latest-emacs-from-source.sh
+++ scripts/install-latest-emacs-from-source.sh
@@ -24,3 +24,12 @@ cd ${ROOTPATH}
 ./configure
 make
 make install
+
+# mac 通过brew方式安装最新的emacs版本
+# 参考：http://emacsredux.com/blog/2015/05/09/emacs-on-os-x/
+brew install emacs --HEAD --with-cocoa   ## 总是安装最新的
+# 源码clone到/Users/aborn/Library/Caches/Homebrew/emacs--git
+# 安装成功后的目录为： /usr/local/Cellar/emacs
+# ./autogen.sh
+# 配置为 ./configure --disable-silent-rules --enable-locallisppath=/usr/local/share/emacs/site-lisp --infodir=/usr/local/Cellar/e
+# make & make install
