f5e83281ae6e4461b4a304cbd6074b2cbe7cdab6
guobao.jiang <loveaborn@foxmail.com>
2017-01-16 13:45:26 +0800

    aborn/log 先判断日志文件是否存在

--- parts/aborn-log.el
+++ parts/aborn-log.el
@@ -5,7 +5,7 @@
 ;;; Code:
 
 (require 'subr-x)
-(defvar ab--log-file-name "~/.spacemacs.d/local/log.txt")
+(defvar aborn--log-file-name "~/.spacemacs.d/local/log.txt")
 
 (defun aborn/log-format (origin)
   "Format `ORIGIN' log with timestamp."
@@ -13,13 +13,17 @@
           origin))
 
 (defun aborn/log (&rest args)
-  "record &rest args as log into file `ab--log-file-name' and
+  "record &rest args as log into file `aborn--log-file-name' and
    each line with timestamp as prefix."
+  (unless (file-exists-p (f-dirname aborn--log-file-name))
+    (message "log file doesn't exists, create its directory %s!"
+             (f-dirname aborn--log-file-name))
+    (mkdir (f-dirname aborn--log-file-name) t))
   (with-temp-buffer
     (goto-char (point-max))
     (insert (aborn/log-format (string-join args " ")))
     (insert "\n")
-    (append-to-file (point-min) (point-max) ab--log-file-name)))
+    (append-to-file (point-min) (point-max) aborn--log-file-name)))
 
 (provide 'aborn-log)
 ;;; aborn-log.el ends here
