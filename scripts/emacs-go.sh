8a744c1a629e2c25c88231917184055c8460e759
guobao.jiang <loveaborn@foxmail.com>
2017-02-04 11:46:06 +0800

    update go installation

--- scripts/emacs-go.sh
+++ scripts/emacs-go.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# 将下面环境变量添加到.zshrc或者.bashrc里
+# export GOPATH=$HOME/Go
+# export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin
+
+# 创建目录
+# mkdir -p $GOPATH
+
 # install spacemacs go pre-requisites
 
 go get -u -v github.com/nsf/gocode
