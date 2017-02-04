1eb4c606a0b469cd7b980736f3b27eca75c7a1fd
guobao.jiang <loveaborn@foxmail.com>
2017-02-04 11:29:27 +0800

    add emacs-go

---
+++ scripts/emacs-go.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# install spacemacs go pre-requisites
+
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/oracle
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v golang.org/x/tools/cmd/goimports
