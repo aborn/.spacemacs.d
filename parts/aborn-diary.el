abf0e9c3db68451d0448e0b5861f81ad49f0e3d3
guobao.jiang <guobao.jiang@dianping.com>
2017-02-13 14:27:03 +0800

    finished aborn/diary-create

--- parts/aborn-diary.el
+++ parts/aborn-diary.el
@@ -9,12 +9,33 @@
 ;;; Code:
 
 
+(defcustom aborn/diary-root-path "~/github/diary"
+  "Root diary path"
+  :group 'aborn
+  :type 'string)
+
+(defun aborn/diary-header-content ()
+  (concat
+   (format "#+TITLE: %s\n" (format-time-string "%Y-%m-%d"))
+   "#+AUTHOR: aborn\n"
+   (format "#+DATE: %s\n" (format-time-string "%Y-%m-%d %H:%M" (current-time)))
+   "#+EMAIL: aborn.jiang@gmail.com\n"
+   "#+LANGUAGE: zh\n"
+   "#+LATEX_HEADER: \\usepackage{xeCJK}\n\n"
+   "#+SETUPFILE: ~/github/org-html-themes/setup/theme-readtheorg.setup\n\n"
+   "-----\n"))
+
 (defun aborn/diary-create ()
   (interactive)
   (let* ((date-name (format-time-string "%Y-%m-%d.org" (current-time)))
          file-name)
     (setq file-name (read-from-minibuffer
                      (format "Diary name (default %s): " date-name) date-name ))
+    (setq file-name (expand-file-name file-name aborn/diary-root-path))
+    (find-file file-name)
+    (unless (file-exists-p file-name)
+      (insert (aborn/diary-header-content)))
+    ;;(save-buffer)
     (message "diary name=%s" file-name)
     ))
 
