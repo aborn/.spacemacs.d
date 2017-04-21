4ace6c01bed767d4ca6f2fb926e6fd117c510285
aborn <aborn>
2017-04-22 00:38:00 +0800

    set wakatime-api-key

--- init-aborn.el
+++ init-aborn.el
@@ -275,6 +275,7 @@
 ;; https://wakatime.com/help/plugins/emacs
 ;; https://wakatime.com/@aborn
 (global-wakatime-mode)
+(setq wakatime-api-key "e191899f-9e2a-4d00-8100-4e9f9523decb")
 ;; https://github.com/wakatime/wakatime-mode/issues/6
 (when (string= system-type "gnu/linux")
   (message "gnu/linux system")
