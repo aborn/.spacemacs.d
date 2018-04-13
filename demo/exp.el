18f72bdadcb59ecd640a92fd0027ee037ec87b67
guobao.jiang <guobao.jiang@dianping.com>
2018-04-13 13:07:28 +0800

    commit by magit in emacs@2018-04-13 13:07:27

--- demo/exp.el
+++ demo/exp.el
@@ -171,3 +171,10 @@
               (setq rlist (cons (buffer-file-name) rlist))
             ))))
     rlist))
+
+
+(defun exp/read-lines (filePath)
+  "Return a list of lines of a file at filePath."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (split-string (buffer-string) "\n" t)))
