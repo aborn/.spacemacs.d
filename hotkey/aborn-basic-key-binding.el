aedb5a7a467e364657f21c484774a29aa9936f5b
aborn <guobao.jiang@aliyun.com>
2024-05-12 12:34:03 +0800

    back and forward

--- hotkey/aborn-basic-key-binding.el
+++ hotkey/aborn-basic-key-binding.el
@@ -37,7 +37,6 @@
 (global-set-key (kbd "C-'") 'move-middle-of-line)
 (global-set-key (kbd "C-;") 'move-forward-by-five)
 (global-set-key (kbd "C-:") 'move-backward-by-five)
-(global-set-key (kbd "C-<") 'pop-global-mark)
 (global-set-key (kbd "<C-tab>") 'bury-buffer)         ; switch buffer C-tab
 
 ;;(global-set-key (kbd "M-m") 'ab/switch-to-shell-buffer)  ; function in switch-swift
@@ -58,6 +57,53 @@
 ;; comment-region and uncomment-region
 (global-set-key (kbd "C-(") 'comment-region)
 (global-set-key (kbd "C-)") 'uncomment-region)
+
+(defun unpop-to-mark-command ()
+  "Unpop off mark ring. Does nothing if mark ring is empty."
+  (interactive)
+  (when mark-ring
+    (setq mark-ring (cons (copy-marker (mark-marker)) mark-ring))
+    (set-marker (mark-marker) (car (last mark-ring)) (current-buffer))
+    (when (null (mark t)) (ding))
+    (setq mark-ring (nbutlast mark-ring))
+    (goto-char (marker-position (car (last mark-ring))))))
+
+;; 快速导航 https://stackoverflow.com/questions/3393834/how-to-move-forward-and-backward-in-emacs-mark-ring
+(global-set-key (kbd "C-<") 'pop-global-mark)
+(global-set-key (kbd "C->") 'unpop-to-mark-command)
+
+;; (defun marker-is-point-p (marker)
+;;   "test if marker is current point"
+;;   (and (eq (marker-buffer marker) (current-buffer))
+;;        (= (marker-position marker) (point))))
+
+;; (defun push-mark-maybe ()
+;;   "push mark onto `global-mark-ring' if mark head or tail is not current location"
+;;   (if (not global-mark-ring) (error "global-mark-ring empty")
+;;     (unless (or (marker-is-point-p (car global-mark-ring))
+;;                 (marker-is-point-p (car (reverse global-mark-ring))))
+;;       (push-mark))))
+
+;; (defun backward-global-mark ()
+;;   "use `pop-global-mark', pushing current point if not on ring."
+;;   (interactive)
+;;   (push-mark-maybe)
+;;   (when (marker-is-point-p (car global-mark-ring))
+;;     (call-interactively 'pop-global-mark))
+;;   (call-interactively 'pop-global-mark))
+
+;; (defun forward-global-mark ()
+;;   "hack `pop-global-mark' to go in reverse, pushing current point if not on ring."
+;;   (interactive)
+;;   (push-mark-maybe)
+;;   (setq global-mark-ring (nreverse global-mark-ring))
+;;   (when (marker-is-point-p (car global-mark-ring))
+;;     (call-interactively 'pop-global-mark))
+;;   (call-interactively 'pop-global-mark)
+;;   (setq global-mark-ring (nreverse global-mark-ring)))
+
+;; (global-set-key [M-left] (quote backward-global-mark))
+;; (global-set-key [M-right] (quote forward-global-mark))
 
 ;; 设置find-file-in-project
 ;; https://github.com/technomancy/find-file-in-project
